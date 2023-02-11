d50266f6ab67f74ba5abf620454355028696082f
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-11 19:26:38 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 
+
+
 function App() {
+
+  let [text, setText] = useState();
+
+  let textHandler = (e) => {
+    setText(e.target.value);
+  }
+
+  let saveText = e => {
+    e.preventDefault();
+    localStorage.setItem('myText',JSON.stringify(text));
+    setText('');
+
+  }
+  let getDateFromLocalStorage = e => {
+    let savedText = JSON.parse(localStorage.getItem('myText'));
+    setText(localStorage.getItem('myText'));
+    setText(savedText);
+  }
+  console.log(localStorage);
   return (
     <div className="App">
-      <div className="box">
-        <div className="field">
-          <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
-          </div>
-        </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
-      </div>
+      <textarea
+        value={text}
+        onChange={textHandler}
+        onLoad={getDateFromLocalStorage}>
+
+      </textarea>
+      <button onClick={saveText}>Save</button>
     </div>
   );
 }
